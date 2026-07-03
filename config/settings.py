c2cb9f7fcfd3e539c60c8fd2db9c3dae260d198d
arya.gilang <arya.gilang@ui.ac.id>
2026-07-03 23:09:09 +0700

    fix: allow all hosts for vercel subdomains revised again

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure-7=i1@@!6-%#6#5&9fxk()k$d95pd8na*^9mvjgiymcq98cs42s
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*',
+'web-porto-h2m06bi5g-arya-gilangs-projects.vercel.app',]
 
 
 # Application definition
