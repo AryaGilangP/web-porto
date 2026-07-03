0aef50a77ce0d0e09cd48e1d9da7b63b1e832b56
arya.gilang <arya.gilang@ui.ac.id>
2026-07-03 23:32:04 +0700

    fix: enforce collectstatic command trigger inside asgi on deployment build

--- config/asgi.py
+++ config/asgi.py
@@ -1,16 +1,20 @@
-"""
-ASGI config for config project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/6.0/howto/deployment/asgi/
-"""
-
+# config/asgi.py
 import os
-
 from django.core.asgi import get_asgi_application
+from django.core.management import call_command
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
+# Jalankan collectstatic otomatis saat Vercel melakukan inisialisasi aplikasi
+try:
+    call_command('collectstatic', '--noinput', '--clear')
+except Exception as e:
+    print(f"Collectstatic error: {e}")
+
 application = get_asgi_application()
+
+# Tetap kunci setting host agar aman dari DisallowedHost
+from django.conf import settings
+settings.ALLOWED_HOSTS = ['*']
+
+app = application
--- config/settings.py
+++ config/settings.py
@@ -116,10 +116,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = '/static/'
+# config/settings.py (Bagian paling bawah)
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'core', 'static'),
 ]
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+# Ganti baris storage kemarin dengan versi ini agar Django toleran jika ada aset yang jalurnya melesat
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
