12405faaf3536e8cf23a21ef79642c275734c99f
arya.gilang <arya.gilang@ui.ac.id>
2026-07-03 23:15:39 +0700

    fix: force inject allowed hosts inside wsgi

--- config/wsgi.py
+++ config/wsgi.py
@@ -7,10 +7,17 @@ For more information on this file, see
 https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 """
 
+# config/wsgi.py
 import os
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 application = get_wsgi_application()
+
+# PENTING: Suntikkan override host secara langsung ke setelan runtime Django
+from django.conf import settings
+settings.ALLOWED_HOSTS = ['*']
+
+# Handler untuk Vercel
+app = application
